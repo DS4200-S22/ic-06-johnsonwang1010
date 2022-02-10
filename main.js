a19902ed5474700328cdc956219ad2b97b3bd376
johnsonwang1010 <62771330+johnsonwang1010@users.noreply.github.com>
2022-02-10 13:30:11 -0500

    2

--- main.js
+++ main.js
@@ -10,5 +10,27 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    console.log("Button 1 was clicked");
+    
+    let buttonDiv = document.getElementById("button-div"); 
+    // then we buttonDiv's content
+    buttonDiv.innerHTML = newText; 
 }
+
+function button2Clicked() {
+    let newText = "Button was clicked!"; 
+    // we need to select the button-div (using its id) 
+    let buttonDiv = document.getElementById("button-div"); 
+    // then we buttonDiv's content
+    buttonDiv.innerHTML = newText; 
+}
+
+
+
+
+
+
+
+let random_num = Math.random()
+console.log(random_num)
+
